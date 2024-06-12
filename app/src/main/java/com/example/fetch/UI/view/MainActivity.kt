78a1520114125d387b36784cae0099b651063ac0
Jair <jaedhc91@hotmail.com>
2024-06-12 13:35:49 -0600

    Primer commit

---
+++ app/src/main/java/com/example/fetch/UI/view/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.fetch.UI.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fetch.R
+import com.example.fetch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            val i = Intent(this, SignUpActivity::class.java)
+            startActivity(i)
+        }
+
+    }
+}
