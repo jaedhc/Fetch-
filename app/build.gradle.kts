2c67d28125babab5500cf6242b2bca6cfa7adf62
Jair Hernandez <jair.hernandez@avattar.com>
2024-06-21 22:04:58 -0600

    Signup ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     // GLIDE DEPENDECY
     implementation("com.github.bumptech.glide:glide:4.14.2")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
